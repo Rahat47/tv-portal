dfc5868c61f570dae8fd2000dba7731bb58b7821
Rahat47 <djrayhan8@gmail.com>
2023-04-26 14:08:18 +0000

    Deploying to gh-pages from @ Rahat47/tv-portal@fbab38729ae45b17c4beaefa23523546b4b16574 🚀

--- src/model/title.ts
+++
@@ -1,9 +0,0 @@
-export abstract class Title {
-  text: string;
-
-  constructor(title: string) {
-    this.text = title;
-  }
-
-  abstract update(title: Title): Title;
-}
--- src/view/pageView.ts
+++
@@ -1,8 +0,0 @@
-export abstract class PageView {
-  abstract render(...args: any[]): void;
-
-  protected clear() {
-    const app = document.getElementById('app') as HTMLElement;
-    app.innerHTML = '';
-  }
-}
